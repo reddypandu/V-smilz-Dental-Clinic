256bc06055e071409a40357f6b4e6bbd1113b2cc
reddypandu <saipandupatt143@gmail.com>
2024-02-13 12:46:41 +0530

    p

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -1,22 +1,41 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-  
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
 }
 
 function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }    
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+// loader
+var loader = function () {
+  // Show loader
+  $("#ftco-loader").addClass("show");
+
+  // Hide loader after document is fully loaded
+  window.onload = function () {
+    setTimeout(function () {
+      if ($("#ftco-loader").length > 0) {
+        $("#ftco-loader").removeClass("show");
+      }
+    }, 1000);
+  };
+};
+
+loader();
