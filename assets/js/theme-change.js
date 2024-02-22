cfc6480683e042d3eb992b4a51df77a8b6fbbbd0
reddypandu <saipandupatt143@gmail.com>
2024-02-22 18:47:25 +0530

    load

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -45,3 +45,27 @@ const btn = document.getElementById("plus");
 btn.addEventListener("click", () => {
   open.classList.toggle("active");
 });
+$(window).on("scroll", function () {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 80) {
+    $("#site-header").addClass("nav-fixed");
+  } else {
+    $("#site-header").removeClass("nav-fixed");
+  }
+});
+
+//Main navigation Active Class Add Remove
+$(".navbar-toggler").on("click", function () {
+  $("header").toggleClass("active");
+});
+$(document).on("ready", function () {
+  if ($(window).width() > 991) {
+    $("header").removeClass("active");
+  }
+  $(window).on("resize", function () {
+    if ($(window).width() > 991) {
+      $("header").removeClass("active");
+    }
+  });
+});
